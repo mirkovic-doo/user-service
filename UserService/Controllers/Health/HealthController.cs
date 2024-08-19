de8443165195a1ff30ce1befdae2ba7338253437
Mico Milic <milicftn@gmail.com>
2024-08-19 19:37:15 +0200

    Setup ci/cd: remove authorize from health check controller

--- UserService/Controllers/Health/HealthController.cs
+++ UserService/Controllers/Health/HealthController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace UserService.Controllers.Health;
 
-[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class HealthController : ControllerBase
