1760168425ba27023cdf78ecc8e0fb675f6ae77a
Mico Milic <milicftn@gmail.com>
2024-08-26 23:09:26 +0200

    Feature 26: Serilog configuration (#27)

---
+++ UserService/Configuration/RequestContextLoggingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Serilog.Context;
+
+namespace UserService.Configuration;
+
+public class RequestContextLoggingMiddleware
+{
+    private const string CorrelationIdHeaderName = "X-Correlation-Id";
+    private readonly RequestDelegate _next;
+
+    public RequestContextLoggingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public Task Invoke(HttpContext context)
+    {
+        var correlationId = GetCorrelationId(context);
+
+        using (LogContext.PushProperty("CorrelationId", correlationId))
+        {
+            return _next.Invoke(context);
+        }
+    }
+
+    private static string GetCorrelationId(HttpContext context)
+    {
+        context.Request.Headers.TryGetValue(
+            CorrelationIdHeaderName, out var correlationId);
+
+        return correlationId.FirstOrDefault() ?? context.TraceIdentifier;
+    }
+}
